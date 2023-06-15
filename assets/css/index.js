bc574102d310ebf6795a6ed2f36bf231e88aa24a
Gloria <gmanzano0616@gmail.com>
2023-06-14 19:12:47 -0500

    js changes

--- assets/css/index.js
+++ assets/css/index.js
@@ -1,6 +1,29 @@
+
+
 function validFormFieldInput(data){
-    const newTaskNameInput = document.querySelector('#task-name');
-    const name = newTaskNameInput.value;
+
+const newTaskNameInput = document.querySelector('#task-name');
+const name = newTaskNameInput.value;
+
+const newTaskDescription = document.querySelector('#task-description');
+const task = newTaskDescription.value;
+
+const newAssignedTo = document.querySelector('#assigned-to');
+const assigned = newAssignedto.value;
 
 }
-console.log("name:  "+ name);
+const errorElement = document.getElementById('error')
+const form = document.getElementById('#form')
+ 
+form.addEventListener('button', (e) => {
+
+let message = []
+if (name.value === '' || name.value === null) {
+    message.push('Name is required')
+}
+if (message.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = message.join(', ')
+}   
+
+})
